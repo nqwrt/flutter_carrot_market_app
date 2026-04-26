b44db6b42ed2043ed53211f448119be4b7b25e7e
heo <admin@admin.com>
2026-04-26 17:20:03 +0900

    이미지 전체 크기로

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -9,12 +9,38 @@ class DetailContentView extends StatefulWidget {
 }
 
 class _DetailContentViewState extends State<DetailContentView> {
+
+  Size? size;
+
+  PreferredSizeWidget _appbarWidget(){
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.share)), //pub.dev 사이트 접속
+        IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+      ]
+    );
+  }
+
+  Widget _bodyWidget(){
+    return Container(
+      child: Image.asset(
+        widget.data["image"].toString(),
+        width: size?.width,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(widget.data["title"] ?? "제목없음"),
-      ),
+    size = MediaQuery.of(context).size;
+
+    return Scaffold(
+        extendBodyBehindAppBar: true, //치고 올라감
+        appBar: _appbarWidget(),
+        body: _bodyWidget()
     );
   }
 }
