1c744f669448335478c4840dc758d900794aa632
heo <admin@admin.com>
2026-04-28 20:48:07 +0900

    밑에 인디케이터 나오기 까지

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ samples, guidance on mobile development, and a full API reference.
 https://parkjh7764.tistory.com/entry/Flutter-%EB%B0%B0%EC%B9%98-%EC%9C%84%EC%A0%AF-Row-Column-Flexible-%EC%9C%84%EC%A0%AF-Expanded-%EC%9C%84%EC%A0%AF-Stack-%EC%9C%84%EC%A0%AF
 https://github.com/sudar-life/flutter_carrot_market
 
-https://www.youtube.com/watch?v=fwzO5QD5Djc&list=PLgRxBCVPaZ_3R0h7mCkLJ1RKh7XRvoZdF&index=5
+https://www.youtube.com/watch?v=fwzO5QD5Djc&list=PLgRxBCVPaZ_3R0h7mCkLJ1RKh7XRvoZdF&index=8
 
 https://github.com/tyeom/flutter_carrot_market
 
--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,58 +1,171 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class DetailContentView extends StatefulWidget {
-  final Map<String,String> data;
-  const DetailContentView({super.key,required this.data});
+  final Map<String, String> data;
+
+  const DetailContentView({super.key, required this.data});
 
   @override
   State<DetailContentView> createState() => _DetailContentViewState();
 }
 
 class _DetailContentViewState extends State<DetailContentView> {
+  List<String> imgList = [];
+  int _current = 0;
 
-  PreferredSizeWidget _appbarWidget(){
+  @override
+  void initState() {
+    super.initState();
+    imgList = [
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+    ];
+    _current = 0;
+    // _loadMyFavoriteContentState();
+  }
+
+  PreferredSizeWidget _appbarWidget() {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.arrow_back),
+        color: Colors.white,
+      ),
       actions: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.share)), //pub.dev 사이트 접속
+        IconButton(onPressed: () {}, icon: Icon(Icons.share)),
+        //pub.dev 사이트 접속
         IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-      ]
+      ],
     );
   }
 
-  Widget _bodyWidget(){
+  Widget _bodyWidget() {
     final size = MediaQuery.of(context).size;
     final tag = widget.data["cid"] ?? widget.data["image"] ?? "default_tag";
-    print("태그"+ tag);
+    print("태그" + tag);
     return Container(
-      child: Hero(
-        tag: tag,
-        child: Image.asset(
-          widget.data["image"].toString(),
-          width: size.width,
-          fit: BoxFit.cover, //이 그러짐 방지
-        ),
+      child: Column(
+        children: [
+          Hero(
+            tag: tag,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                height: size.width,
+                initialPage: 0,
+                enableInfiniteScroll: false,
+                viewportFraction: 1,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+              items: imgList.map((url) {
+                print("이미지 경로: $url");  // 👈 추가
+                return Image.asset(
+                    url,
+                    fit: BoxFit.cover,
+                    width: size.width,
+                );
+              }).toList(),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                // onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: (Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black)
+                          .withValues(alpha: _current == entry.key ? 0.9 : 0.4)),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
 
   _bottomBarWidget() {
     final size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width,
-      height: 55,
-      color: Colors.red,
-    );
+    return Container(width: size.width, height: 55, color: Colors.red);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true, //치고 올라감
-        appBar: _appbarWidget(),
-        body: _bodyWidget(),
-        bottomNavigationBar: _bottomBarWidget(),
+      extendBodyBehindAppBar: true, //치고 올라감
+      appBar: _appbarWidget(),
+      body: _bodyWidget(),
+      bottomNavigationBar: _bottomBarWidget(),
     );
   }
 }
 //https://www.youtube.com/watch?v=c6rwfZBKjp8&list=PLgRxBCVPaZ_3R0h7mCkLJ1RKh7XRvoZdF&index=8
+
+//https://pub.dev/packages/carousel_slider
+
+
+// class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
+//   int _current = 0;
+//   final CarouselSliderController _controller = CarouselSliderController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Carousel with indicator controller demo')),
+//       body: Column(children: [
+//         Expanded(
+//           child: CarouselSlider(
+//             items: imageSliders,
+//             carouselController: _controller,
+//             options: CarouselOptions(
+//                 autoPlay: true,
+//                 enlargeCenterPage: true,
+//                 aspectRatio: 2.0,
+//                 onPageChanged: (index, reason) {
+//                   setState(() {
+//                     _current = index;
+//                   });
+//                 }),
+//           ),
+//         ),
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: imgList.asMap().entries.map((entry) {
+//             return GestureDetector(
+//               onTap: () => _controller.animateToPage(entry.key),
+//               child: Container(
+//                 width: 12.0,
+//                 height: 12.0,
+//                 margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+//                 decoration: BoxDecoration(
+//                     shape: BoxShape.circle,
+//                     color: (Theme.of(context).brightness == Brightness.dark
+//                         ? Colors.white
+//                         : Colors.black)
+//                         .withValues(alpha: _current == entry.key ? 0.9 : 0.4)),
+//               ),
+//             );
+//           }).toList(),
+//         ),
+//       ]),
+//     );
+//   }
+// }
